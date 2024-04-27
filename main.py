27f7120b09dabb0519ccb21ab61d678db4b02ffa
windexlight <windexlight@gmail.com>
2024-04-27 15:50:19 -0700

    Use scancodes instead of Qt key codes.

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from PyQt5.QtWidgets import (QDialog, QFileDialog, QHeaderView, QInputDialog,
                              QLabel, QMessageBox, QTableWidgetItem, QAbstractButton, QCheckBox)
 from windowMain import Ui_Form
 from keynames import keyNames
+from scancodenames import scancodes
 
 class mainWindow(QtWidgets.QMainWindow):
     def __init__(self):
@@ -17,7 +18,6 @@ class mainWindow(QtWidgets.QMainWindow):
         self.ui.setupUi(self)
         self.keysPressed = []
         self.setFocusPolicy(Qt.StrongFocus)
-        # QtWidgets.qApp.focusChanged.connect(self.on_focusChanged)       
 
     def focusOutEvent(self, event):
         self.keysPressed.clear()
@@ -25,21 +25,27 @@ class mainWindow(QtWidgets.QMainWindow):
 
     def keyPressEvent(self, event: QKeyEvent):
         if not event.isAutoRepeat():
-            k = event.key()
+            k = event.nativeScanCode() #.key()
+            if k not in scancodes:
+                print(F"Unrecognized scancode {k} pressed")
+                return
             if k not in self.keysPressed:
                 self.keysPressed.append(k)
             self.updateKeysPressed()
-            print(F"{keyNames[k]} pressed")
+            print(F"{scancodes[k]} pressed")
 
     def keyReleaseEvent(self, event: QKeyEvent):
         if not event.isAutoRepeat():
-            k = event.key()
+            k = event.nativeScanCode() #.key()
+            if k not in scancodes:
+                print(F"Unrecognized scancode {k} released")
+                return
             self.keysPressed = [i for i in self.keysPressed if i != k]
             self.updateKeysPressed()
-            print(F"{keyNames[k]} released")
+            print(F"{scancodes[k]} released")
 
     def sayKeysPressed(self):
-        return '+'.join(map(lambda k: keyNames[k], self.keysPressed))
+        return '+'.join(map(lambda k: scancodes[k], self.keysPressed))
     
     def updateKeysPressed(self):
         self.ui.label_keyPrompt.setText(self.sayKeysPressed())
